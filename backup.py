7bae05df2e27734d094f87ce8e7d892ec06a644a
Lefteris Dalamagas <lefteris.dalamagas@gmail.com>
2013-10-01 16:02:53 +0300

    Added configuration file

--- backup.py
+++ backup.py
@@ -1,7 +1,5 @@
+import ConfigParser
 import subprocess
-
-__author__ = 'ldalamagas'
-
 import logging
 import tarfile
 import os
@@ -9,28 +7,38 @@ from datetime import datetime, timedelta
 import ftplib
 import re
 
-# Backup configuration
-backup_items = ["/home/ldalamagas/playground/backup"]
-backup_prefix = "backup."
-backup_suffix = ".tar.gz"
-retention_period = 0     # In days, None to disable the feature
-tmp_dir = "/tmp"
+__author__ = 'ldalamagas'
 
-# MySQL BackUp
-db_names = ["opinions"]        # None to disable the feature
-db_host = "localhost"
-db_username = "opinions"
-db_password = "opinions"
+config = {}
 
-# Remote Storage
-remote_host = "ftp.imc.com.gr"
-remote_dir = "python_backup"
-remote_user = "developer"
-remote_password = "developer"
+
+def read_config(config):
+
+    cp = ConfigParser.ConfigParser()
+    cp.readfp(open("backup.cfg"))
+
+    # General backup configuration
+    config["backup_items"] = (cp.get("general", "backup_items")).split(",")
+    config["backup_prefix"] = cp.get("general", "backup_prefix")
+    config["backup_suffix"] = cp.get("general", "backup_suffix")
+    config["retention_period"] = cp.getint("general", "retention_period")
+    config["tmp_dir"] = cp.get("general", "tmp_dir")
+
+    # MySQL
+    config["db_names"] = (cp.get("general", "db_names")).split(",")
+    config["db_host"] = cp.get("general", "db_host")
+    config["db_user"] = cp.get("general", "db_user")
+    config["db_password"] = cp.get("general", "db_password")
+
+    # Remote Storage
+    config["ftp_host"] = cp.get("general", "ftp_host")
+    config["ftp_dir"] = cp.get("general", "ftp_dir")
+    config["ftp_user"] = cp.get("general", "ftp_user")
+    config["ftp_password"] = cp.get("general", "ftp_password")
 
 
 def get_tar_name():
-    return ''.join([backup_prefix, datetime.now().date().strftime("%Y%m%d"), backup_suffix])
+    return ''.join([config["backup_prefix"], datetime.now().date().strftime("%Y%m%d"), config["backup_suffix"]])
 
 
 def on_error(start_time):
@@ -41,23 +49,24 @@ def on_error(start_time):
 
 
 def main():
+    read_config(config)
     cleanup = []
     logging.basicConfig(level=logging.INFO, format="%(asctime)s - %(levelname)s - %(message)s")
     start_time = datetime.now()
     logging.info("starting backup %s", start_time.time().strftime("%H:%M:%S"))
     tar_file = get_tar_name()
-    tar_path = os.path.join(tmp_dir, tar_file)
+    tar_path = os.path.join(config["tmp_dir"], tar_file)
 
     # Dump MySQL databases
-    if db_names is not None:
+    if config["db_names"] is not "-1":
         try:
             logging.info("dumping databases")
-            for db_name in db_names:
+            for db_name in config["db_names"]:
                 logging.info("dumping database %s", db_name)
                 cmd = "mysqldump --single-transaction --host %s -u %s -p%s %s" \
-                      % (db_host, db_username, db_password, db_name)
-                dumpfile_name = os.path.join(tmp_dir, ".".join([db_name, "sql"]))
-                backup_items.append(dumpfile_name)      # Include it to the backup archive
+                      % (config["db_host"], config["db_user"], config["db_password"], db_name)
+                dumpfile_name = os.path.join(config["tmp_dir"], ".".join([db_name, "sql"]))
+                config["backup_items"].append(dumpfile_name)      # Include it to the backup archive
                 cleanup.append(dumpfile_name)           # We are gonna need this to cleanup later
                 dumpfile = open(dumpfile_name, "w")     # Sample: /tmp/database.sql
                 subprocess.check_call(cmd, stdout=dumpfile, shell=True)
@@ -74,9 +83,9 @@ def main():
 
     # Archive the directories
     try:
-        logging.info("archiving files %s", backup_items)
+        logging.info("archiving files %s", config["backup_items"])
         tar = tarfile.open(tar_path, "w:gz")
-        for item in backup_items:
+        for item in config["backup_items"]:
             tar.add(item)
         tar.close()
         cleanup.append(tar_path)
@@ -86,30 +95,30 @@ def main():
 
     # Transfer archive to remote destination
     try:
-        logging.info("transferring %s to %s/%s", tar_path, remote_host, remote_dir)
-        ftp = ftplib.FTP(remote_host, remote_user, remote_password)
-        ftp.cwd(remote_dir)
+        logging.info("transferring %s to %s/%s", tar_path, config["ftp_host"], config["ftp_dir"])
+        ftp = ftplib.FTP(config["ftp_host"], config["ftp_user"], config["ftp_password"])
+        ftp.cwd(config["ftp_dir"])
         f = open(tar_path, "rb")
         ftp.storbinary("".join(["STOR ", tar_file]), f)
     except ftplib.Error:
-        logging.error("error while transferring %s archive to %s/%s", tar_path, remote_host, remote_dir)
+        logging.error("error while transferring %s archive to %s/%s", tar_path, config["ftp_host"], config["ftp_dir"])
         ftp.close()
         on_error(start_time)
     finally:
         f.close()
 
     # Delete old archives
-    if retention_period is not None:
+    if config["retention_period"] is not -1:
         try:
             nothing_deleted = True
-            logging.info("deleting archives older than %i days", retention_period)
+            logging.info("deleting archives older than %i days", config["retention_period"])
             file_listing = ftp.nlst()
-            regex = re.compile(r"" + backup_prefix + "(\d{8})" + backup_suffix)
+            regex = re.compile(r"" + config["backup_prefix"] + "(\d{8})" + config["backup_suffix"])
 
             for backup_file in file_listing:
                 date_string = re.findall(regex, backup_file)
                 backup_date = datetime.strptime(date_string[0], "%Y%m%d")
-                if (start_time - backup_date) > timedelta(retention_period):
+                if (start_time - backup_date) > timedelta(config["retention_period"]):
                     ftp.delete(backup_file)
                     nothing_deleted = False
                     logging.info("'%s' deleted", backup_file)
