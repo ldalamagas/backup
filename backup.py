1ef84ce4701ddedb18760bbc7589fc5f24873966
Lefteris Dalamagas <lefteris.dalamagas@gmail.com>
2013-10-03 14:54:54 +0300

    Corrected a bug while reporting ftp error

--- backup.py
+++ backup.py
@@ -114,17 +114,12 @@ def main():
             logger.info("dumping databases")
             for db_name in config["db_names"]:
                 logger.info("dumping database %s", db_name)
-                #cmd = "mysqldump --single-transaction --host %s -u %s -p%s %s" \
-                #      % (config["db_host"], config["db_user"], config["db_password"], db_name)
                 dumpfile_name = os.path.join(config["tmp_dir"], ".".join([db_name, "sql"]))
                 cmd = "mysqldump --single-transaction --host %s -u %s -p%s %s > %s" \
                       % (config["db_host"], config["db_user"], config["db_password"], db_name, dumpfile_name)
                 config["backup_items"].append(dumpfile_name)    # Include it to the backup archive
                 cleanup.append(dumpfile_name)                   # We are gonna need this to cleanup later
-                # dumpfile = open(dumpfile_name, "w")             # Sample: /tmp/database.sql
-                # subprocess.check_call(cmd, stdout=dumpfile, shell=True)
                 subprocess.check_output(cmd, stderr=subprocess.STDOUT, shell=True)
-                # dumpfile.close()
         except subprocess.CalledProcessError as error:
             on_error(error, "error while dumping mysql databases, "
                             "the error occurred while calling the mysqldump process")
